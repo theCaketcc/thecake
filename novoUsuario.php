b59f32455be79f1a2eaac1934f1c01ae0ae5915b
EQUIPE3_3F <EQUIPE3_3F@LAB2M28.itbeng.inf>
2014-04-28 10:39:50 -0300

    THE CAKE

--- novoUsuario.php
+++ novoUsuario.php
@@ -1,19 +1,23 @@
 <?php
+
 echo '<meta charset=UTF-8>';
 include_once 'conexao/conecta.inc';
-$email = $_POST['login'];
-$senha = $_POST['senha'];
-$nome = $_POST['nome'];
-$tipoUsuario = 'RES';
-$query = "INSERT INTO usuario (EMAIL_USUARIO,SENHA_USUARIO,NOME_USUARIO,TIPO_USUARIO)";
-$query.= " VALUES('$email','$senha','$nome','$tipoUsuario')";
+$email =$_POST['login'];
+$senha =$_POST['senha'];
+$nome=$_POST['nome'];
+$tipoUsuario='RES';
+$query="INSET INTO usuario( EMAIL_USUARIO, SENHA_USUARIO, NOME_USUARIO, TIPO_USUARIO)";
+$query.= " VALUES('$email', '$senha', '$nome', '$tipo_usuario')";
 
-if(mysql_query($query)){
-    echo '<script>alert("Usuário cadastrado com sucesso !") </script>';
-    echo '<a href=frmlogin.php>Efetuar login</a>';
-    }else{
-        echo '<script>
-                 alert("Não foi possível seu cadastro!")
-                 location.href=index.php
-              </script>';
+if(mysql_querry ($query)){
+    
+  echo '<script>alert("Usuário cadastrado com sucesso! ")</script>';
+echo'<a href=frmlogin.php>Efetuar login</a>';}
+else
+{ echo'<script>
+     alert("Não foi possível seu cadastro!")
+     location.href=index.php
+         </script>';
+    
+    
 }
